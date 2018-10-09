495b09ddb67af80e7413e8233000bd3e576d34e0
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-09 17:08:57 +0530

    Created organization component

---
+++ src/Organization.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization: </strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+    </div>
+  );
+};
+
+export default Organization;
