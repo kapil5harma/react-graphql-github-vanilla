67bee5d01469fcbe234e337af12083ba169ac0b7
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-09 18:26:10 +0530

    Issues component modified to include the issues list with reactions

--- src/Issues.js
+++ src/Issues.js
@@ -6,6 +6,11 @@ const Issues = ({ issues }) => {
       {issues.edges.map(issue => (
         <li key={issue.node.id}>
           <a href={issue.node.url}>{issue.node.title}</a>
+          <ul>
+            {issue.node.reactions.edges.map(reaction => (
+              <li key={reaction.node.id}>{reaction.node.content}</li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
