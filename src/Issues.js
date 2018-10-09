39b536dbb4abd7640c588ff975a7f952792fde1e
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-10-09 17:33:14 +0530

    Created Issues component

---
+++ src/Issues.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Issues = ({ issues }) => {
+  return (
+    <ul>
+      {issues.edges.map(issue => (
+        <li key={issue.node.id}>
+          <a href={issue.node.url}>{issue.node.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Issues;
